2915d87d0e19b40751ab8bb66b1d86d18a18e38e
Frank Hangler <fhangler@gmail.com>
2017-06-29 13:11:04 -0700

    Use random number generation for sample plots

--- demo/demo-src.js
+++ demo/demo-src.js
@@ -4,13 +4,37 @@ const ReactDOM = require('react-dom');
 
 function App(props) {
 
+    // Get an integer in the range of min, max, inclusive.
+    // From https://stackoverflow.com/a/1527820
+    const getRandomInt = (min=1, max=9) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    const getRandomXY = (min=1, max=5) => {
+        return { x: getRandomInt(min, max), y: getRandomInt(min, max) };
+    };
+
+    const getArrayOfRandomXY = (length=5, min=1, max=5) => {
+        const array = [];
+        for (let i = 0; i < length; i++) { array.push(getRandomXY(min, max)); }
+        return array;
+    };
+
+    const getArrayOfLinearY = (length=5, min=1, max=5) => {
+        const array = [];
+        for (let i = 0; i < length; i++) {
+            array.push({ x: i, y: getRandomInt(min, max)});
+        }
+        return array;
+    };
+
     const barChartData = [
-        { category: 'Apples', count: 3, color: 'rgba(0, 150, 0, 0.5)' },
-        { category: 'Bananas', count: 5, color: '#fe0' },
-        { category: 'Oranges', count: 2, color: 'orange' },
-        { category: 'Raspberries', count: 9, color: '#e25098' },
-        { category: 'Strawberries', count: 7, color: 'rgb(255, 0, 0)' },
-        { category: 'Watermelons', count: 4, color: 'green' },
+        { category: 'Apples', count: getRandomInt(), color: 'rgba(0, 150, 0, 0.5)' },
+        { category: 'Bananas', count: getRandomInt(), color: '#fe0' },
+        { category: 'Oranges', count: getRandomInt(), color: 'orange' },
+        { category: 'Raspberries', count: getRandomInt(), color: '#e25098' },
+        { category: 'Strawberries', count: getRandomInt(), color: 'rgb(255, 0, 0)' },
+        { category: 'Watermelons', count: getRandomInt(), color: 'green' },
     ];
     const barChartDataForDisplay = barChartData.map((item, index) => {
         return (
@@ -18,11 +42,9 @@ function App(props) {
         );
     });
 
-    const randomData = [
-        1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5,
-        5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 9
-    ];
-    const histogramData = PlusPlot.Histogram.defaultBinning(randomData);
+    let histogramData = [];
+    for (let i = 0; i < 100; i++) { histogramData.push(getRandomInt(1, 9)); }
+    histogramData = PlusPlot.Histogram.defaultBinning(histogramData);
     histogramData.color = '#fc0';
     const histogramDataForDisplay = histogramData.map((item, index) => {
         return (
@@ -37,13 +59,8 @@ function App(props) {
         );
     });
 
-    const scatterPlotData = [
-        { x: 1, y: 3, color: 'black' }, { x: 5, y: 7 },
-        { x: 3, y: 1 }, { x: 4, y: 5 },
-        { x: 6, y: 5 }, { x: 6, y: 1 },
-        { x: 4, y: 4 }, { x: 3, y: 8 },
-        { x: 7, y: 2 }, { x: 2, y: 5 },
-    ];
+    const scatterPlotData = [];
+    for (let i = 0; i < 20; i++) { scatterPlotData.push(getRandomXY(1, 9)); }
     const scatterPlotDataForDisplay = scatterPlotData.map((item, index) => {
         return (
             <span key={index}>&nbsp;&nbsp;{JSON.stringify(item, null, 1)}<br /></span>
@@ -51,9 +68,9 @@ function App(props) {
     });
 
     const lineChartData = [
-        { color: 'blue', values: [ {x: 0, y: 1}, {x: 1, y: 3}, {x: 2, y: 0} ]},
-        { color: 'red', values: [ {x: 0, y: 3}, {x: 1, y: 1}, {x: 2, y: 3} ]},
-        { color: 'green', values: [ {x: 0, y: 4}, {x: 1, y: 3}, {x: 2, y: 1} ]},
+        { color: 'blue', values: getArrayOfLinearY() },
+        { color: 'red', values: getArrayOfLinearY() },
+        { color: 'green', values: getArrayOfLinearY() },
     ];
     const lineChartDataForDisplay = lineChartData.map((item, index) => {
         return (
